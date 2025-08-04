8d7b0c220a206d78b1432932cc32e8d219d17e9e
Daniel Hansson <mailto@danielhansson.nu>
2025-08-04 12:59:50 +0200

    Create exceptions.py

---
+++ bazarr/api/plex/exceptions.py
@@ -0,0 +1,29 @@
+"""Plex authentication exceptions."""
+
+class PlexAuthError(Exception):
+    """Base exception for Plex authentication errors."""
+    def __init__(self, message, status_code=500, error_code=None):
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
+        self.error_code = error_code
+
+class InvalidTokenError(PlexAuthError):
+    """Raised when Plex token is invalid."""
+    def __init__(self, message="Invalid Plex token"):
+        super().__init__(message, status_code=401, error_code="INVALID_TOKEN")
+
+class TokenExpiredError(PlexAuthError):
+    """Raised when Plex token has expired."""
+    def __init__(self, message="Plex token has expired"):
+        super().__init__(message, status_code=401, error_code="TOKEN_EXPIRED")
+
+class PlexConnectionError(PlexAuthError):
+    """Raised when cannot connect to Plex."""
+    def __init__(self, message="Cannot connect to Plex"):
+        super().__init__(message, status_code=503, error_code="CONNECTION_ERROR")
+
+class PlexServerNotFoundError(PlexAuthError):
+    """Raised when Plex server not found."""
+    def __init__(self, message="Plex server not found"):
+        super().__init__(message, status_code=404, error_code="SERVER_NOT_FOUND")
